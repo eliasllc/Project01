4d666e04503f5edf6ddd5bc2cc43c1bc3c7288dc
eliasllc <90351293+eliasllc@users.noreply.github.com>
2021-09-08 21:27:54 -0400

    Update main.cpp
    mis datos

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-using namespace std;
+
 int main() {
-	cout << "hola mundo";
+	std::cout << "Mi nombre : Elias Llanos Contreras\n";
+	std::cout << "Mi cu: 35-4907\n";
+	std::cout << "Mi celular: 63743477\n";
+	std::cout << "Mi carrera: Ingenieria de sistemas\n";
+	return 0;
 }
